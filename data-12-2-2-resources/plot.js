2fbf7744c6b1341f46fe67c8d85e946fc02ed068
AngG17 <95720986+AngG17@users.noreply.github.com>
2022-03-11 22:12:35 -0600

    Add files via upload

---
+++ data-12-2-2-resources/plot.js
@@ -0,0 +1,57 @@
+console.log(cityGrowths);
+
+var sortedCities = cityGrowths.sort((a,b) =>
+a.Increase_from_2016 - b.Increase_from_2016).reverse();
+console.log(sortedCities);
+
+var topFiveCities = sortedCities.slice(0,5);
+console.log(topFiveCities);
+
+var topFiveCityNames = cityGrowths.map(city => city.City);
+var topFiveCityGrowths = cityGrowths.map(city => parseInt(city.Increase_from_2016));
+console.log(topFiveCityNames);
+console.log(topFiveCityGrowths);
+
+var topFiveCityNames = topFiveCities.map(city => city.City);
+var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
+console.log(topFiveCityNames);
+console.log(topFiveCityGrowths);
+parseInt();
+
+var trace = {
+    x: topFiveCityNames,
+    y: topFiveCityGrowths,
+    type: "bar"
+  };
+  var data = [trace];
+  var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: {title: "City" },
+    yaxis: {title: "Population Growth, 2016-2017"}
+  };
+  Plotly.newPlot("bar-plot", data, layout);
+
+// Seven largest cities
+
+var topSevenCities = sortedCities.slice(0,7);
+console.log(topSevenCities);
+
+
+var topSevenCityNames = topSevenCities.map(city => city.City);
+var topSevenCityGrowths = topSevenCities.map(city => parseInt(city.Increase_from_2016));
+console.log(topSevenCityNames);
+console.log(topSevenCityGrowths);
+parseInt();
+
+var trace = {
+    x: topSevenCityNames,
+    y: topSevenCityGrowths,
+    type: "bar"
+  };
+  var data = [trace];
+  var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: {title: "City" },
+    yaxis: {title: "Population Growth, 2016-2017"}
+  };
+  Plotly.newPlot("bar-plot", data, layout);
